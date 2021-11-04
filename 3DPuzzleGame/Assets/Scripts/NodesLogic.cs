378906d845b08a5ac219cecfa18df14d9f250762
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2021-11-04 01:16:33 -0500

    Correct the scenes nodes

--- 3DPuzzleGame/Assets/Scripts/NodesLogic.cs
+++ 3DPuzzleGame/Assets/Scripts/NodesLogic.cs
@@ -6,10 +6,11 @@ public class NodesLogic : MonoBehaviour
 {
     void OnMouseDown()
     {
+        bool canMoveHere = gameObject.CompareTag("Node");
         bool thereObjectAbove = Physics.Raycast(transform.position, transform.up, 2);
         Vector3 positionToMove = new Vector3(transform.position.x, 0.5f, transform.position.z);
 
-        if (ObjectToMove.objectToMove != null && thereObjectAbove == false)
+        if (canMoveHere && ObjectToMove.objectToMove != null && thereObjectAbove == false)
         {
             ObjectToMove.markToMove.position = positionToMove;
             ObjectToMove.objectToMove.position = positionToMove;
